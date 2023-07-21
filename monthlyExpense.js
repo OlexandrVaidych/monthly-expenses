e985be4d54a2140c9caa13bcb7ac0cfc4e8ae2c5
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-07-21 17:38:49 +0300

    monthly expense calculated

---
+++ monthlyExpense.js
@@ -0,0 +1,13 @@
+function calculateMonthlyExpense(){
+    var foodPrice = parseFloat(document.getElementById("food").value)
+    var internetPrice = parseFloat(document.getElementById("internet").value)
+    var lightPrice = parseFloat(document.getElementById("light").value)
+    var tariffPrice = parseFloat(document.getElementById("tariff").value)
+    var houseRentPrice = parseFloat(document.getElementById("house").value)
+    var clothesPrice = parseFloat(document.getElementById("clothes").value)
+    var restPrice = parseFloat(document.getElementById("rest").value)
+
+    monthlyExpense = foodPrice + internetPrice + lightPrice + tariffPrice + houseRentPrice + clothesPrice + restPrice
+    
+    alert(monthlyExpense)
+}
