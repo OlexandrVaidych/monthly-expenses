0d204b3fe976f21792d460cd2fac1a3066ec8b33
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-07-24 12:49:12 +0300

    var changed on const

--- monthlyExpense.js
+++ monthlyExpense.js
@@ -1,11 +1,11 @@
 function calculateMonthlyExpense(){
-    var foodPrice = parseFloat(document.getElementById("food").value)
-    var internetPrice = parseFloat(document.getElementById("internet").value)
-    var lightPrice = parseFloat(document.getElementById("light").value)
-    var tariffPrice = parseFloat(document.getElementById("tariff").value)
-    var houseRentPrice = parseFloat(document.getElementById("house").value)
-    var clothesPrice = parseFloat(document.getElementById("clothes").value)
-    var restPrice = parseFloat(document.getElementById("rest").value)
+    const foodPrice = parseFloat(document.getElementById("food").value)
+    const internetPrice = parseFloat(document.getElementById("internet").value)
+    const lightPrice = parseFloat(document.getElementById("light").value)
+    const tariffPrice = parseFloat(document.getElementById("tariff").value)
+    const houseRentPrice = parseFloat(document.getElementById("house").value)
+    const clothesPrice = parseFloat(document.getElementById("clothes").value)
+    const restPrice = parseFloat(document.getElementById("rest").value)
 
     monthlyExpense = foodPrice + internetPrice + lightPrice + tariffPrice + houseRentPrice + clothesPrice + restPrice
     
